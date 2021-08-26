4034e9431247b4971c33e75a3ae560a42f2314d6
vinodanola <vino.danola@gmail.com>
2021-08-26 09:02:34 +0700

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { makeStyles } from '@material-ui/core/styles';
 import './style.css';
-import HomePage from './views/HomePage/HomePage.js';
+import ReposPage from './views/ReposPage/ReposPage.js';
 
 const hist = createBrowserHistory();
 
@@ -12,7 +12,7 @@ export default function App() {
     <div className="App">
       <Router history={hist}>
         <Switch>
-          <Route path="/" render={props => <HomePage {...props} />} />
+          <Route path="/" render={props => <ReposPage {...props} />} />
         </Switch>
       </Router>
     </div>
